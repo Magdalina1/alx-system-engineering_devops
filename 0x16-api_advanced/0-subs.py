f58839ba8ee9e9ba9392b3607e9beb0fa90e5240
Magdalina1 <majdalol95@gmail.com>
2024-01-16 11:22:10 -0800

    fix the code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ query Reddit API and return number of subscribers for given subreddit
-    If an invalid subreddit is given, the function should return 0.
-    """
-    url = "http://www.reddit.com/r/{:s}/about.json".format(subreddit)
-    headers = {'user-agent': 'egsyquest'}
-    r = requests.get(url, headers=headers)
-
-    if (r.status_code is 302):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    if (r.status_code is 404):
-        return 0
-
-    return r.json()['data'].get('subscribers', 0)
+    results = response.json().get("data")
+    return results.get("subscribers")
